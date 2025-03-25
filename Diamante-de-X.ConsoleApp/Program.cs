97aceea595eca7259d9eb37b040a9ea9b622ee3a
Agatha Sates <satesagatha@gmail.com>
2025-03-25 15:59:35 -0300

    Add project files.

---
+++ Diamante-de-X.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Diamante_de_X.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
