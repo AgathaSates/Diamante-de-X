8b5439eebb72b930039c216dfc233901e4e080db
Agatha Sates <satesagatha@gmail.com>
2025-03-26 02:16:19 -0300

    feat: Criado classe EscrevaInteração

---
+++ Diamante-de-X.ConsoleApp/EscrevaInteracao.cs
@@ -0,0 +1,24 @@
+﻿namespace Diamante_de_X.ConsoleApp;
+
+class EscrevaInteracao
+{
+    public static bool GerarDiamanteNovamente()
+    {
+        Thread.Sleep(1000);
+        Console.Write("Deseja montar outro diamante? (S/N): ");
+
+        Validadores.SimouNaoValido();
+
+        if (Validadores.opcaoContinuar == "N")
+            return false;
+
+        else
+            return true;
+    }
+
+    public static void PedirEntrada()
+    {
+        Console.Write("-> Digite um número ímpar entre 3 e 51: ");
+        Validadores.valorValido = Validadores.EhValido();
+    }
+}
