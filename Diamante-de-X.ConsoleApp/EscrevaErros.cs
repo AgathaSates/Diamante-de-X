84228538423dc6b4c5e773dc94af50a126d7aa47
Agatha Sates <satesagatha@gmail.com>
2025-03-26 03:11:13 -0300

    style: colorida interação com o usúario.

--- Diamante-de-X.ConsoleApp/EscrevaErros.cs
+++ Diamante-de-X.ConsoleApp/EscrevaErros.cs
@@ -5,22 +5,22 @@ class EscrevaErros
     public static void NaoEstaNoIntervalo()
     {
         Console.Clear();
-        Console.Write($"(X) {Validadores.valorValido} não está entre 3 e 51, digite novamente: ");
+        Colorir.EscreverEmVermelho($"(X) {Validadores.valorValido} não está entre 3 e 51, digite novamente: ");
     }
 
     public static void NaoehImpar() 
     {
         Console.Clear();
-        Console.Write($"(X) {Validadores.valorValido} não é ímpar, digite novamente: ");
+        Colorir.EscreverEmVermelho($"(X) {Validadores.valorValido} não é ímpar, digite novamente: ");
     }
 
     public static void NaoehINT() 
     {
-        Console.Write($"(X) {Validadores.valorDigitado} não é um número inteiro, Digite novamente: ");
+        Colorir.EscreverEmVermelho($"(X) {Validadores.valorDigitado} não é um número inteiro, Digite novamente: ");
     }
 
     public static void NaoehSimouNao() 
     {
-        Console.Write("(X) Opção inválida! Digite novamente S ou N: ");
+        Colorir.EscreverEmVermelho("(X) Opção inválida! Digite novamente S ou N: ");
     }
 }
