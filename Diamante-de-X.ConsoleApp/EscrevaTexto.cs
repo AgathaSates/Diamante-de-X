4aa2efc52c02b15e60ed90d71705bc6ebc7ed7c5
Agatha Sates <satesagatha@gmail.com>
2025-03-26 02:16:44 -0300

    feat: Criado classe EscrevaTexto.

---
+++ Diamante-de-X.ConsoleApp/EscrevaTexto.cs
@@ -0,0 +1,25 @@
+﻿namespace Diamante_de_X.ConsoleApp;
+
+class EscrevaTexto
+{
+    public static void ExibirTitulo()
+    {
+        Console.Clear();
+        Console.WriteLine("---------------------------------------------------------");
+        Console.WriteLine("               Diamante de X");
+        Console.WriteLine("---------------------------------------------------------");
+    }
+
+    public static void ExibirDiamante()
+    {
+        Console.Clear();
+        ExibirTitulo();
+        Console.WriteLine("Gerando Diamante...");
+        Thread.Sleep(1150);
+
+        Console.WriteLine("---------------------------------------------------------");
+        Diamante.DesenharDiamante();
+        Console.WriteLine("---------------------------------------------------------");
+    }
+
+}
