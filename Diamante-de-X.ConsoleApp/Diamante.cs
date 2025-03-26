42b49b9d95b93aca38d874c7e3a2151807b2e7c3
Agatha Sates <satesagatha@gmail.com>
2025-03-26 02:14:36 -0300

    feat: Criado classe Diamante.

---
+++ Diamante-de-X.ConsoleApp/Diamante.cs
@@ -0,0 +1,18 @@
+﻿namespace Diamante_de_X.ConsoleApp;
+
+class Diamante
+{
+    public const int limitedodiamante = 51;
+
+    public static void DesenharDiamante()
+    {
+        for (int i = 0; i < Validadores.valorValido; i++)
+        {
+            if (i <= Validadores.valorValido / 2)
+                Console.WriteLine(new string(' ', Validadores.valorValido / 2 - i) + new string('x', 2 * i + 1));
+
+            else      
+                Console.WriteLine(new string(' ', i - Validadores.valorValido / 2) + new string('x', 2 * (Validadores.valorValido - i) - 1));
+        }
+    }
+}
