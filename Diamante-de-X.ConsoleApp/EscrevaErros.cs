c7a1431a421c079ddd6587525824d45f557b03a4
Agatha Sates <satesagatha@gmail.com>
2025-03-26 02:15:15 -0300

    feat: Criado classe EscrevaErros.

---
+++ Diamante-de-X.ConsoleApp/EscrevaErros.cs
@@ -0,0 +1,26 @@
+﻿namespace Diamante_de_X.ConsoleApp;
+
+class EscrevaErros
+{
+    public static void NaoEstaNoIntervalo()
+    {
+        Console.Clear();
+        Console.Write($"(X) {Validadores.valorValido} não está entre 3 e 51, digite novamente: ");
+    }
+
+    public static void NaoehImpar() 
+    {
+        Console.Clear();
+        Console.Write($"(X) {Validadores.valorValido} não é ímpar, digite novamente: ");
+    }
+
+    public static void NaoehINT() 
+    {
+        Console.Write($"(X) {Validadores.valorDigitado} não é um número inteiro, Digite novamente: ");
+    }
+
+    public static void NaoehSimouNao() 
+    {
+        Console.Write("(X) Opção inválida! Digite novamente S ou N: ");
+    }
+}
