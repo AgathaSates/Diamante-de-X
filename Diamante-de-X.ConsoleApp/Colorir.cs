e49e5f1c548c2dab9b3a32e3288d745e2aaec180
Agatha Sates <satesagatha@gmail.com>
2025-03-26 03:10:16 -0300

    feat: Adicionado classe colorir

---
+++ Diamante-de-X.ConsoleApp/Colorir.cs
@@ -0,0 +1,37 @@
+﻿namespace Diamante_de_X.ConsoleApp;
+
+class Colorir
+{
+    public static void EscreverEmVermelho(string mensagem)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.Write(mensagem);
+        Console.ResetColor();
+    }
+
+    public static void EscreverEmAmarelo(string mensagem)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write(mensagem);
+        Console.ResetColor();
+    }
+    public static void EscreverCharEmAmarelo(char c)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write(c);
+        Console.ResetColor();
+    }
+
+    public static void EscreverEmAzulEscuro(string mensagem)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.WriteLine(mensagem);
+        Console.ResetColor();
+    }
+    public static void EscreverEmCiano(string mensagem)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine(mensagem);
+        Console.ResetColor();
+    }
+}
