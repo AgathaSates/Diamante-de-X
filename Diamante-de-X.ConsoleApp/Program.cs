50a90d51fed81b2a17b05879089292161d9f857d
Agatha Sates <satesagatha@gmail.com>
2025-03-25 23:38:41 -0300

    feat: implementado Loop e verificação de número int.

--- Diamante-de-X.ConsoleApp/Program.cs
+++ Diamante-de-X.ConsoleApp/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Diamante_de_X.ConsoleApp;
+﻿namespace Diamante_de_X.ConsoleApp;
 
 internal class Program
 {
@@ -8,45 +6,72 @@ internal class Program
     static void Main(string[] args)
     {
         const int limitedodiamante = 51;
-
-        Console.WriteLine("---------------------------------------------------------");
-        Console.WriteLine("               Diamante de X");
-        Console.WriteLine("---------------------------------------------------------");
-
-        Console.Write("-> Digite um número ímpar entre 1 e 51: ");
-        int valorDigitado = Convert.ToInt32(Console.ReadLine());
-
-        while (valorDigitado % 2 == 0)
+        int valorValido;
+        while (true)
         {
             Console.Clear();
-            Console.Write($"(X) {valorDigitado} não é ímpar, digite novamente: ");
-            valorDigitado = Convert.ToInt32(Console.ReadLine());
-        }
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("               Diamante de X");
+            Console.WriteLine("---------------------------------------------------------");
 
-        while (valorDigitado < 0 || valorDigitado > limitedodiamante) 
-        {
+            Console.Write("-> Digite um número ímpar entre 1 e 51: ");
+            string valorDigitado = Console.ReadLine();
+
+            while (true)
+            {
+               
+                if (string.IsNullOrWhiteSpace(valorDigitado) || !int.TryParse(valorDigitado, out valorValido))
+                {
+                    Console.Write(" -> (X) Valor inválido! Digite novamente: ");
+                    valorDigitado = Console.ReadLine();
+                }
+                else
+                    break;
+            }
+
+            while (valorValido % 2 == 0)
+            {
+                Console.Clear();
+                Console.Write($"(X) {valorValido} não é ímpar, digite novamente: ");
+                valorValido = Convert.ToInt32(Console.ReadLine());
+            }
+
+            while (valorValido < 0 || valorValido > limitedodiamante)
+            {
+                Console.Clear();
+                Console.Write($"(X) {valorValido} não está entre 1 e 51, digite novamente: ");
+                valorValido = Convert.ToInt32(Console.ReadLine());
+            }
+
             Console.Clear();
-            Console.Write($"(X) {valorDigitado} não está entre 1 e 51, digite novamente: ");
-            valorDigitado = Convert.ToInt32(Console.ReadLine());
-        }
+            Console.WriteLine("Gerando Diamante...");
+            Thread.Sleep(1150);
 
-        Console.Clear() ;
-        Console.WriteLine("Gerando Diamante...");
-        Thread.Sleep(1150);
-
-        Console.WriteLine("---------------------------------------------------------");
-        for (int i = 0; i < valorDigitado; i++)
-        {
-            if (i <= valorDigitado / 2)
+            Console.WriteLine("---------------------------------------------------------");
+            for (int i = 0; i < valorValido; i++)
             {
-                Console.WriteLine(new string(' ', valorDigitado / 2 - i) + new string('x', 2 * i + 1));
-            }
+                if (i <= valorValido / 2)
+                {
+                    Console.WriteLine(new string(' ', valorValido / 2 - i) + new string('x', 2 * i + 1));
+                }
 
-            else
-            {
-                Console.WriteLine(new string(' ', i - valorDigitado / 2) + new string('x', 2 * (valorDigitado - i) - 1));
+                else
+                {
+                    Console.WriteLine(new string(' ', i - valorValido / 2) + new string('x', 2 * (valorValido - i) - 1));
+                }
             }
+            Console.WriteLine("---------------------------------------------------------");
+
+            Thread.Sleep(1000);
+            Console.Write("Deseja efetuar outro calculo? (S/N) ");
+            string opcaoContinuar = Console.ReadLine()!.ToUpper();
+            while (string.IsNullOrWhiteSpace(opcaoContinuar) || opcaoContinuar.ToUpper() != "S" && opcaoContinuar.ToUpper() != "N")
+            {
+                Console.Write(" -> (X) Opção inválida! Digite novamente: ");
+                opcaoContinuar = Console.ReadLine()!;
+            }
+            if (opcaoContinuar == "N")
+               break;
         }
-        Console.WriteLine("---------------------------------------------------------");
     }
 }
